a4194a07b6762c271aa870f4d2d69c15ca6e246b
Jacob3251 <jacobfrye3251@gmail.com>
2023-02-15 11:55:35 +0600

    teacher post manager ui done

--- src/components/AdminDashboard/TeacherPostManager.js
+++ src/components/AdminDashboard/TeacherPostManager.js
@@ -6,6 +6,8 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase.init";
 import useUniversityPost from "../../hooks/useUniversityPost";
 import { CiPen, CiTrash } from "react-icons/ci";
+import { FaPencilAlt, FaTrashAlt } from "react-icons/fa";
+import { Puff } from "react-loader-spinner";
 const TeacherPostBox = ({
   title,
   name,
@@ -36,27 +38,32 @@ const TeacherPostBox = ({
     window.location.reload();
   };
   return (
-    <div className="bg-[#628e90] text-white my-8 p-5 rounded-md ">
-      <div className="flex justify-between items-center">
-        <h3 className="font-bold text-2xl">{title}</h3>
-        <div className="bg-white flex py-2 rounded-xl px-2">
-          <CiPen className="text-2xl text-[#3c2317] p-1 h-10 w-10 rounded-full hover:bg-green-500 hover:text-white duraiton-300"></CiPen>
-          <CiTrash
-            onClick={() => handlePostDelete(id)}
-            className="text-2xl text-[#3c2317] p-1 h-10 w-10 rounded-full hover:bg-red-500 hover:text-white duraiton-300"
-          ></CiTrash>
+    <div className="bg-white text-[#36454f] mb-8 p-5 shadow-md shadow-gray-500 hover:drop-shadow-md">
+      <div className=" ">
+        <div className="flex justify-between">
+          <div className="flex flex-col my-3 ">
+            <b className="text-[#dc4734] text-[14px]">
+              Type: <span className="text-[#36454f]">{type}</span>
+            </b>
+            {/* Time will go below here ============================ */}
+            <i className="text-[10px]">Posted at: {time}</i>
+          </div>
+          <div className="flex py-2 space-x-2">
+            <div className="text-[18px] text-[#36454f] rounded-full hover:bg-[#dc4734] hover:text-white duration-200 flex justify-center items-center px-3">
+              <FaPencilAlt></FaPencilAlt>
+            </div>
+            <div className="text-[18px] text-[#36454f] rounded-full hover:bg-[#dc4734] hover:text-white duration-200 flex justify-center items-center px-3">
+              <FaTrashAlt onClick={() => handlePostDelete(id)}></FaTrashAlt>
+            </div>
+          </div>
         </div>
+        <h3 className="font-bold text-[14px]">{title}</h3>
       </div>
-      <div className="flex flex-col my-3">
-        <b className="text-[#3c2317] text-lg">Type:{type}</b>
-        {/* Time will go below here ============================ */}
-        <i className="text-xs">Posted at: {time}</i>
-      </div>
-      <p className="text-xs">
+
+      <p className="text-[10px] font-bold text-[#36454f] mt-3">
         Published By: <i>{name}</i>
-        Published By: <i>{id}</i>
       </p>
-      <p className="mb-5">{content}</p>
+      <p className="mb-5 text-[14px]">{content}</p>
     </div>
   );
 };
@@ -123,61 +130,82 @@ const TeacherPostManager = () => {
   };
   //   const [post] = useUniversityPost();
   return (
-    <div>
-      <h3 className="text-[#3c2317] text-2xl font-bold text-center mb-10 ">
-        Teacher Post Submission Form
-      </h3>
-      <form onSubmit={handleSubmit} ref={postInput}>
-        <input
-          className="w-full my-2 py-3 pl-3 rounded-md"
-          type="text"
-          name="title"
-          placeholder="Enter Post Title"
-        />
-        <input
-          className="w-full my-2 py-3 pl-3 rounded-md"
-          type="text"
-          name="content"
-          placeholder="Enter Post Details"
-        />
-        <input
-          className="w-full my-2 py-3 pl-3 rounded-md"
-          type="text"
-          name="type"
-          placeholder="Enter Post Type"
-        />
-        <input
-          className="w-full my-2 py-3 pl-3 rounded-md"
-          type="text"
-          name="teacherCode"
-          placeholder="Enter Teacher Code"
-        />
-        <input
-          className="w-full my-2 py-3 bg-[#628e90] hover:bg-[#3c2317] text-white text-lg font-bold scale-95 hover:scale-100 duration-200 rounded-md"
-          type="submit"
-          value="Submit"
-        />
-      </form>
-      {teacherNormalPostLoading ? (
-        <div>Loading Teacher Posts</div>
-      ) : (
-        <div className="">
-          {normalPosts.map((post) => (
-            <TeacherPostBox
-              name={post.name}
-              title={post.title}
-              id={post._id}
-              dept={post.dept}
-              content={post.content}
-              email={post.email}
-              type={post.type}
-              time={post.time ? post.time : null}
-            >
-              {}
-            </TeacherPostBox>
-          ))}
+    <div className="flex  items-start space-x-5 justify-center ">
+      <div className="w-full">
+        {teacherNormalPostLoading ? (
+          <div className="h-[90vh] w-full flex flex-col justify-center items-center">
+            <Puff
+              height="80"
+              width="80"
+              radius={1}
+              color="#3c2317"
+              ariaLabel="puff-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+            <h3 className="animate-bounce400 font-bold text-lg mt-2">
+              Loading
+            </h3>
+          </div>
+        ) : (
+          <div className="w-full">
+            {normalPosts.map((post) => (
+              <TeacherPostBox
+                name={post.name}
+                title={post.title}
+                id={post._id}
+                dept={post.dept}
+                content={post.content}
+                email={post.email}
+                type={post.type}
+                time={post.time ? post.time : null}
+              >
+                {}
+              </TeacherPostBox>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="w-full px-5 mx-auto">
+        <div className="bg-white shadow-md shadow-gray-500 hover:drop-shadow-md">
+          <h3 className="text-[#36454f] text-[14px] font-bold text-center mb-5 pt-5">
+            Teacher Post Submission Form
+          </h3>
+          <form onSubmit={handleSubmit} ref={postInput} className="px-2">
+            <input
+              className="w-full mb-2 py-3 pl-3 shadow-gray-500 shadow-inner"
+              type="text"
+              name="title"
+              placeholder="Enter Post Title"
+            />
+            <input
+              className="w-full my-2 py-3 pl-3 shadow-gray-500 shadow-inner"
+              type="text"
+              name="content"
+              placeholder="Enter Post Details"
+            />
+            <input
+              className="w-full my-2 py-3 pl-3 shadow-gray-500 shadow-inner"
+              type="text"
+              name="type"
+              placeholder="Enter Post Type"
+            />
+            <input
+              className="w-full my-2 py-3 pl-3 shadow-gray-500 shadow-inner"
+              type="text"
+              name="teacherCode"
+              placeholder="Enter Teacher Code"
+            />
+            <input
+              className="w-full my-2 py-3 bg-[#dc4734] hover:bg-white text-white hover:text-[#dc4734] border-2 border-[#dc4734] text-[14px] font-bold  duration-200 "
+              type="submit"
+              value="SUBMIT"
+            />
+          </form>
         </div>
-      )}
+      </div>
+
       {/* <button onClick={handleSubmit}>Add post university</button> */}
     </div>
   );
